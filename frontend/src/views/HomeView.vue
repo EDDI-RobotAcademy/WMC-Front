b5ae400ea6ff9cb299376c2e63b8e0bb843a46ea
jonginhan <instantgogo@gmail.com>
2023-03-18 04:28:14 +0900

    feat: [한종인] 사용자 회원가입(sign up) 페이지 UI [WMC-UI 1]

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,19 +3,18 @@
     <div id="home">
       <nav>
         <router-link to="/">Home</router-link> |
+        <router-link to="/sign-up">SignUpTest</router-link> |
       </nav>
       <router-view/>
     </div>
   </v-container>
 </template>
-
 <script>
 
-export default {
-  name: 'HomeView',
+  export default {
+    name: 'Home',
 
-  components: {
-  },
-}
-
+    components: {
+    },
+  }
 </script>
