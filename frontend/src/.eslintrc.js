765ebccd9c6d6d1c046fc13e650c5258d5ae353e
jonginhan <instantgogo@gmail.com>
2023-03-18 04:20:33 +0900

    init: vuetify 및 저장소 초기설정

--- frontend/src/.eslintrc.js
+++ frontend/src/.eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
 	env: {
-		browser: true,
-		es2021: true
+	  browser: true,
+	  es2021: true,
 	},
-	extends: [
-		'plugin:vue/essential',
-		'standard'
-	],
+	extends: ['plugin:vue/essential', 'standard'],
 	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module'
+	  ecmaVersion: 12,
+	  sourceType: 'module',
 	},
-	plugins: [
-		'vue'
-	],
+	plugins: ['vue'],
 	rules: {
-		'vue/multi-word-component-names': ['error', {
-			ignores: ['Home']
-		}],
-        'eslint-disable-next-line': "off",
-        'eslint-disable': "off"
-	}
-}
+	  'vue/multi-word-component-names': [
+		'error',
+		{
+		  ignores: ['Home'],
+		},
+	  ],
+	},
+  };
+  
