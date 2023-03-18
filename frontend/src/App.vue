eba4e87e859254035ac6ac4b4cfd643f5fa05cf1
gomgomehoya <gomgomehoya@gmail.com>
2023-03-18 22:48:47 +0900

    feat: [김호유] 메인화면 UI 추가
    헤더 푸터 큰틀 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,16 +4,22 @@
     <v-main>
       <router-view/>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
-
+import Footer from '@/components/main/layoutForm/Footer.vue';
+import NavigationMenuPage from '@/components/main/layoutForm/navigation/NavigationMenuPage.vue';
+//import NavigationView from '@/views/lecture/layoutForm/navigation/NavigationView.vue';
 export default {
   name: 'App',
-
   data: () => ({
     //
   }),
+  components: {
+    NavigationMenuPage,
+    Footer
+  },
 };
 </script>
---
+++ frontend/src/components/main/layoutForm/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-footer inset padless class="justify-center pl-0">
+      <v-card flat class="text-center" width="100%">
+          <v-divider></v-divider>
+          <v-card-text class="grey--text">
+              <v-btn v-for="icon in icons" :key="icon" class="mx-2" icon>
+                  <v-icon size="16px">
+                      {{ icon }}
+                    </v-icon>
+                </v-btn>
+                <br>
+                Copyright - (주)WMC. All right reserved.
+          </v-card-text>
+        
+      </v-card>
+    </v-footer>
+    
+  </template>
+  
+  <script>
+  export default {
+      name: "Footer",
+      data: () => ({
+          icons: [
+              "mdi-facebook",
+              "mdi-twitter",
+              "mdi-google-plus",
+              "mdi-linkedin",
+              "mdi-instagram"
+          ]
+      })
+  }
+  </script>
+  
+  <style>
+  </style>
