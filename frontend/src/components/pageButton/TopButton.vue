07c91c14f1521e49d0d78c28eac3a25712676b98
Yoon Yongjin <abcd950217@gamil.com>
2023-03-20 19:46:21 +0900

    add: [윤용진]  드레프트 테스트

---
+++ frontend/src/components/pageButton/TopButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button v-if="isTop" @click="scrollTop">Top</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        isTop: true
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      this.isTop = window.scrollY === 0
+    },
+    scrollTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
