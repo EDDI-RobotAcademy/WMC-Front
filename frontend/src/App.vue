c85d53e4ad3ee02a43e3391d0f5207b75ad3c5ee
gomgomehoya <gomgomehoya@gmail.com>
2023-03-21 15:48:50 +0900

    hotfix:[김호유] vue 2 에서는 사용할수 없는 create vue 기능이 존재함
    vue 3 에서만 구동 가능한 기능을 사용하고 있어서 나타난 compile error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,3 +23,54 @@ export default {
   },
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.dropbtn {
+  background-color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 70px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: rgb(200, 200, 200);
+}
+</style>
