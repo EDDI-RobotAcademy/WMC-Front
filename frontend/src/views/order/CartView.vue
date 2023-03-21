90d594f2b226a01cbdfa9f30819889e960defa6d
jonginhan <instantgogo@gmail.com>
2023-03-21 21:09:09 +0900

    feat: [한종인] 사용자 카트 페이지 UI[WMC-UI 12]

---
+++ frontend/src/views/order/CartView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="detailWrap mt-15" >
+   <cart-form @deleteCartItem="deleteCartItem"/>
+  </div>
+</template>
+
+<script>
+import CartForm from "@/components/order/CartForm";
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
