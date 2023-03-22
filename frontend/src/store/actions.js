f8500b14409f01c380b843b960ce3ff9b47d588a
jonginhan <instantgogo@gmail.com>
2023-03-23 03:19:36 +0900

    update: [한종인] 판매자가 상품을 등록할 수 있는 UI [WMC-UI 6]

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,28 +1,34 @@
-import {
-    REQUEST_PRODUCT_LIST_TO_SPRING,
-
-} from './mutation-types'
-import axios from 'axios'
+import { REQUEST_PRODUCT_LIST_TO_SPRING } from './mutation-types';
+import axios from 'axios';
 
 export default {
-    requestCreateProductToSpring ({}, payload) {
+  requestCreateProductToSpring({}, payload) {
+    const { name, description, price, stock, files } = payload;
+    let formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('price', price);
+    formData.append('stock', stock);
+    for (let idx = 0; idx < files.length; idx++) {
+      formData.append('imageDataList[' + idx + ']', files[idx]);
+    }
+    return axios
+      .post('http://localhost:7777/product/register', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(() => {
+        alert('상품 등록 성공!');
+      })
+      .catch(() => {
+        alert('문제 발생!');
+      });
+  },
 
-        const { productName, content, writer, price } = payload
-        return axios.post('http://localhost:7777/product/register',
-            { productName, content, writer, price })
-            .then(() => {
-                alert('상품 등록 성공!')
-            })
-            .catch(() => {
-                alert('문제 발생!')
-            })
-    },
-
-    requestProductListToSpring ({ commit }) {
-        return axios.get('http://localhost:7777/product/list')
-            .then((res) => {
-                commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data)
-            })
-    },
-
-}
+  requestProductListToSpring({ commit }) {
+    return axios.get('http://localhost:7777/product/list').then((res) => {
+      commit(REQUEST_PRODUCT_LIST_TO_SPRING, res.data);
+    });
+  },
+};
