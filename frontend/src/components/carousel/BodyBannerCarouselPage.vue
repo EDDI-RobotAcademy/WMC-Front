a0cdbe600a4866a40ea8f7e353558c4c54b6e33b
복구 <voock9@gmail.com>
2023-03-22 17:46:11 +0900

    add: [김다연] 사용자가 홈페이지에 들어오면 보이는 메인화면 CAROUSEL UI(WMC-UI11)

---
+++ frontend/src/components/carousel/BodyBannerCarouselPage.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="banner-collection">
+    <div class="banner-box">
+      <div class="flex-box">
+        <div class="a">
+          <div class="box a-box">
+            <router-link :to="{ name: 'MonthDrinkView'}" style="text-decoration: none">
+              <v-img
+                  height="300px"
+                  :src="require('@/assets/image/1.jpg')">
+                <div class="text-box">
+                  <p>이달의 신상품</p>
+                  <div class="text">
+                    <p style="margin-top: 10px">상품보기</p>
+                  </div>
+                </div>
+              </v-img>
+            </router-link>
+          </div>
+        </div>
+        <div class="l">
+          <div class="box l-box">
+            <router-link :to="{ name: 'MonthDrinkView'}" style="text-decoration: none">
+              <v-img
+                  height="300px"
+                  :src="require('@/assets/image/1.jpg')">
+                <div class="text-box">
+                  <p>이달의 신상품</p>
+                  <div class="text">
+                    <p style="margin-top: 10px">상품보기</p>
+                  </div>
+                </div>
+              </v-img>
+            </router-link>
+          </div>
+        </div>
+        <div class="r">
+          <div class="box r-box">
+            <router-link :to="{ name: 'FoundryView'}" style="text-decoration: none">
+              <v-img
+                  height="300px"
+                  :src="require('@/assets/image/2.jpg')">
+                <div class="text-box">
+                  <p>이달의 신상품 2</p>
+                  <div class="text">
+                    <p style="margin-top: 10px">더 알아보기</p>
+                  </div>
+                </div>
+              </v-img>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.banner-collection {
+  background-color: #f6f6f6;
+  margin-top: 80px;
+  padding: 80px 0;
+}
+.banner-box {
+  max-width: 1040px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 20px;
+}
+.flex-box {
+  display: flex;
+  justify-content: space-between;
+}
+.flex-box .l {
+  flex: 0 0 50%;
+  width: 50%;
+}
+.flex-box .box {
+  position: relative;
+  box-sizing: border-box;
+}
+.flex-box .r {
+  flex: 0 0 48.33%;
+  width: 48.33%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.box p {
+  font-weight: bold;
+  font-size: 20px;
+  color: #4d5450;
+}
+.box .text-box {
+  margin: 80px 60px 0
+}
+.box .text-box .text {
+  width: 121px;
+  height: 52px;
+  text-align: center;
+  border-radius: 12px;
+  border: 1px solid #4d5450;
+}
+</style>
