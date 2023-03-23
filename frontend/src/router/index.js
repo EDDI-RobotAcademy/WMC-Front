ef1f2eca6caa32f35e786876504af39ac468a99d
jonginhan <instantgogo@gmail.com>
2023-03-23 20:50:25 +0900

    chore: [한종인] 머지중 작은 변경사항들 커밋

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,9 +3,10 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUpView from '@/views/account/SignUpView.vue'
 import SignInView from '@/views/account/SignInView.vue'
-import CartView from '@/views/order/CartView.vue'
 import ProductRegisterPage from '@/views/product/ProductRegisterPage.vue'
 import ProductListPage from '@/views/product/ProductListPage.vue'
+import MyPageView from '@/views/myPage/MyPageView.vue'
+import CartView from "@/views/order/CartView";
 
 Vue.use(VueRouter)
 
@@ -25,11 +26,11 @@ const routes = [
     name: 'SignInView',
     component: SignInView
   },
-  // {
-  //   path: '/my-page',
-  //   name: 'MyPageView',
-  //   component: MyPageView
-  // },
+  {
+    path: '/my-page',
+    name: 'MyPageView',
+    component: MyPageView
+  },
   {
     path: "/cart",
     name: "CartView",
