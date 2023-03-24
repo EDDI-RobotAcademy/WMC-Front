bfc8f975ca546165ec1fcdc36f227bfc6652a44d
Yoon Yongjin <abcd950217@gamil.com>
2023-03-24 21:46:38 +0900

    feat: [윤용진] 공지사항 등록 페이지 UI [WMC-UI 21]

---
+++ frontend/src/components/csCenter/notice/NoticeRegisterForm.vue
@@ -0,0 +1,81 @@
+<template>
+    <form @submit.prevent="onSubmit" class="notice-form">
+      <v-table>
+        <tr>
+          <td>제목</td>
+          <td>
+            <input type="text" v-model="title" class="mt-5 notice-input"/>
+          </td>
+        </tr>
+        <tr>
+          <td>작성자</td>
+          <td>
+            <input type="text" v-model="writer" class="mt-5 mb-5 notice-input"/>
+          </td>
+        </tr>
+        <tr>
+          <td>본문</td>
+          <td>
+            <textarea cols="50" rows="20" v-model="content"  class="mb-5 notice-textarea"/>
+          </td>
+        </tr>
+      </v-table>
+      <div>
+        <button type="submit" class="mr-5 notice-button">등록</button>
+        <router-link :to="{ name: 'NoticeListPage' }" class="notice-button">
+          취소
+        </router-link>
+        
+      </div>
+    </form>
+  </template>
+  
+  <script>
+  
+  export default {
+      name: "NoticeRegisterForm",
+      data () {
+          return {
+              title: '제목을 입력하세요.',
+              writer: 'WMC',
+              content: '내용을 입력하세요.'
+          }
+      },
+      methods: {
+          onSubmit () {
+              const { title, writer, content } = this
+              this.$emit('submit', { title, writer, content })
+          }
+      }
+  }
+  </script>
+  
+  <style scoped>
+  
+  .notice-input {
+  width: 100%;
+  padding: 5px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+.notice-textarea {
+  width: 100%;
+  padding: 5px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+.notice-button {
+  background-color: #428bca;
+  color: #fff;
+  padding: 5px 10px;
+  font-size: 16px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+  
+  </style>
---
+++ frontend/src/views/csCenter/notice/NoticeRegisterPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <h2>공지사항 작성</h2>
+        <notice-register-form @submit="onSubmit" />
+    </v-container>
+</template>
+  
+<script>
+
+import NoticeRegisterForm from '../../../components/csCenter/notice/NoticeRegisterForm.vue'
+import { mapActions } from 'vuex'
+
+export default {
+    components: { NoticeRegisterForm },
+    name: "NoticeListPage",
+    methods: {
+        ...mapActions ([
+            'requestCreateNoticeToSpring'
+        ]),
+        async onSubmit (payload) {
+            await this.requestCreateBoardToSpring(payload)
+            await this.$router.push({
+                name: 'NoticeListPage'
+            })
+        }
+    }
+}
+</script>
+  
+<style></style>
