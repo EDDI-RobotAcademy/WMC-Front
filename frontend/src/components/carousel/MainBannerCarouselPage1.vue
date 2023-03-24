56800dbb6db4d8a08c702f0eb7753a092a10342b
복구 <voock9@gmail.com>
2023-03-24 21:24:04 +0900

    feat:[김다연] 사용자가 홈페이지에 들어오면 보이는 메인화면 CAROUSEL UI(WMC-UI11)

---
+++ frontend/src/components/carousel/MainBannerCarouselPage1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+          <v-carousel hide-delimiters cycle :interval="3000">
+            <v-carouserl-items>
+              <v-carousel-item src="@/assets/banner/banner1.jpg" cover />
+              <v-carousel-item src="@/assets/banner/banner2.jpg" cover />
+              <v-carousel-item src="@/assets/banner/banner3.jpg" cover />
+              <v-carousel-item src="@/assets/banner/banner4.jpg" cover />
+              <v-carousel-item src="@/assets/banner/banner5.jpg" cover />
+            </v-carouserl-items>
+          </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.carousel {
+  width: 100%;
+  margin: 0 auto;
+}
+
+.carousel-inner {
+  width: 100%;
+}
+
+.carousel-item img {
+  width: 100%;
+  height: 300px
+  00px;
+}
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,40 @@
 <template>
-  <v-container>
-    <div id="home">
-      <nav>
-        <router-link to="/"></router-link>
-        <router-link to="/product-list-page">상품 게시판</router-link> 
-        <ProductListPage/>
-      </nav>
-      <router-view/>
-    </div>
-    <v-main>
-      <MainBannerCarouselPage/>
-      <BodyBannerCarouselPage/> 
-    </v-main>
-  </v-container>
+ <div>
+  <div>
+    <MainBannerCarouselPage1/>
 
+  </div>
+  <div>
+    <v-container>
+      <div id="home">
+        <nav>
+          <router-link to="/"></router-link>
+          <router-link to="/product-list-page">상품 게시판</router-link><br>
+          <router-link to="/review-list-page">리뷰 게시판</router-link> 
+
+        </nav>
+        <router-view/>
+      </div>
+    </v-container>
+  </div>
+  <div>
+    <BodyBannerCarouselPage/> 
+  </div>
+</div> 
 </template>
 <script>
-import MainBannerCarouselPage from '@/components/carousel/MainBannerCarouselPage.vue';
+import MainBannerCarouselPage1 from '@/components/carousel/MainBannerCarouselPage1.vue';
 import BodyBannerCarouselPage from '@/components/carousel/BodyBannerCarouselPage.vue';
 import ProductListPage from '@/views/product/ProductListPage.vue'
+import ReviewListPage from '@/components/reviewBoard/ReviewListForm.vue';
 
   export default {
     name: 'Home',
 
     components: {
-     MainBannerCarouselPage,
+     MainBannerCarouselPage1,
      BodyBannerCarouselPage,
+     ReviewListPage,
      ProductListPage,
      
     },
