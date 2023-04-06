d5a6f9daaf44c148b87a27a6af633913c5114053
jonginhan <instantgogo@gmail.com>
2023-04-07 05:07:43 +0900

    update:[한종인] 상품 상세 페이지에서 장바구니 추가하기 [WEB-UI 47]

--- src/components/product/ProductDetailForm.vue
+++ src/components/product/ProductDetailForm.vue
@@ -14,31 +14,115 @@
       </div>
     </v-col>
     <v-col cols="12" sm="6" md="6" v-if="product">
-      <h2>{{ product.name }}</h2>
-      <p>{{ product.description }}</p>
-      <p>{{ product.price }}₩</p>
-      <p>{{ product.stock }} in stock</p>
+      <v-card flat>
+        <v-card-title>{{ product.name }}</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>{{ product.description }}</v-card-text>
+        <v-divider></v-divider>
+        <v-card-subtitle>{{ product.price }}₩</v-card-subtitle>
+        <v-divider></v-divider>
+        <v-card-text>{{ stockStatus }}</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn small color="primary" outlined>Like</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn small color="secondary" @click="addToCart(product)"
+            >Add to Cart</v-btn
+          >
+        </v-card-actions>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   props: {
     product: Object,
   },
   data() {
     return {
-      mainImage: this.product ? this.getImagePath(this.product.imageDataList[0].imageData) : '',
+      mainImage: this.product
+        ? this.getImagePath(this.product.imageDataList[0].imageData)
+        : '',
+      memberId: localStorage.getItem('memberId'),
+      authorityName: localStorage.getItem('authorityName'),
+      cart: [],
     };
+  },
+  computed: {
+    ...mapState(['isAuthenticated']),
+    stockStatus() {
+      if (this.product.stock === 0) {
+        return 'Out of stock';
+      } else if (this.product.stock < 10) {
+        return 'Running out of stock';
+      } else {
+        return '';
+      }
+    },
+  },
+  mounted() {
+    if (localStorage.getItem('userInfo')) {
+      this.$store.state.isAuthenticated = true;
+    } else {
+      this.$store.state.isAuthenticated = false;
+    }
   },
   methods: {
     getImagePath(imageData) {
-      console.log('imageData:', imageData);
       return require(`@/${imageData}`);
     },
     changeMainImage(image) {
       this.mainImage = image;
+    },
+    addToCart(product, quantity = 1) {
+      if (!this.isAuthenticated) {
+        alert('로그인 먼저 하세용^_^');
+        this.$router.push('/sign-in');
+        return;
+      }
+
+      if (this.memberId && this.authorityName) {
+        const cartKey = `cart_${this.memberId}`;
+        let cart = localStorage.getItem(cartKey);
+        if (!cart) {
+          cart = [];
+        } else {
+          cart = JSON.parse(cart);
+        }
+
+        const existingCartItem = cart.find(
+          (item) => item.product_id === product.productId
+        );
+
+        if (existingCartItem) {
+          existingCartItem.quantity += quantity;
+        } else {
+          cart.push({
+            product_id: product.productId,
+            name: product.name,
+            image: product.imageDataList[0].imageData
+              ? this.getImagePath(product.imageDataList[0].imageData)
+              : '',
+            price: product.price,
+            quantity,
+          });
+        }
+
+        localStorage.setItem(cartKey, JSON.stringify(cart));
+        console.log('Cart:', cart);
+        alert('장바구니에 추가되었습니다!');
+      } else {
+        console.log('not added');
+        console.log(this.memberId);
+        console.log(this.authorityName);
+      }
+    },
+    getImagePath(imageData) {
+      return require(`@/${imageData}`);
     },
   },
 };
--- src/views/product/ProductDetailPage.vue
+++ src/views/product/ProductDetailPage.vue
@@ -1,42 +1,32 @@
 <template>
-  <v-container>
-    <product-detail-form :product="product" />
-  </v-container>
-</template>
-
-<script>
-import { mapActions } from 'vuex';
-import ProductDetailForm from '@/components/product/ProductDetailForm.vue';
-
-export default {
-  components: { ProductDetailForm },
-  data() {
-    return {
-      product: null,
-    };
-  },
-  beforeRouteEnter(to, from, next) {
-    const productId = to.params.productId;
-
-    import('@/store').then(({ default: store }) => {
-      store
-        .dispatch('productModule/fetchProductById', productId)
-        .then((product) => {
-          next((vm) => {
-            vm.product = product;
-          });
-        })
-        .catch((error) => {
-          console.error('Error fetching product:', error);
-          next(false);
-        });
-    });
-  },
-
-  methods: {
-    ...mapActions('productModule', ['fetchProductById']),
-  },
-};
-</script>
-
-<style></style>
+    <v-container>
+      <product-detail-form :product="product" />
+    </v-container>
+  </template>
+  
+  <script>
+  import { mapActions } from 'vuex';
+  import ProductDetailForm from '@/components/product/ProductDetailForm.vue';
+  
+  export default {
+    components: { ProductDetailForm },
+    data() {
+      return {
+        product: null,
+      };
+    },
+    mounted() {
+      this.fetchProduct();
+    },
+    methods: {
+      ...mapActions('productModule', ['fetchProductById']),
+      async fetchProduct() {
+        const productId = this.$route.params.productId;
+        this.product = await this.fetchProductById(productId);
+      },
+    },
+  };
+  </script>
+  
+  <style></style>
+  
