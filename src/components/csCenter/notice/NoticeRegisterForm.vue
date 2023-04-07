951edf2c802b8fe08265dc5c9563dd62559d84ff
Yoon Yongjin <abcd950217@gamil.com>
2023-04-08 00:55:45 +0900

    update: [윤용진] 공지사항 등록 페이지 UI [WMC-UI 21]

--- src/components/csCenter/notice/NoticeRegisterForm.vue
+++ src/components/csCenter/notice/NoticeRegisterForm.vue
@@ -10,13 +10,19 @@
       <tr>
         <td>작성자</td>
         <td>
-          <input type="text" v-model="writer" class="mt-5 mb-5 notice-input"/>
+          <input type="text" v-model="Writer" class="mt-5 mb-5 notice-input"/>
         </td>
       </tr>
       <tr>
         <td>본문</td>
         <td>
           <textarea cols="50" rows="20" v-model="content"  class="mb-5 notice-textarea"/>
+        </td>
+      </tr>
+      <tr>
+        <td>파일 업로드</td>
+        <td>
+          <input type="file" id="file" multiple show-size @change="handleFileUpload($event)" class="mb-5"/>
         </td>
       </tr>
     </table>
@@ -33,22 +39,27 @@
 <script>
 
 export default {
-  name: 'NoticeRegisterForm',
-  data () {
-    return {
-      title: '제목을 입력하세요.',
-      writer: 'WMC',
-      content: '내용을 입력하세요.',
-      files: []
-    }
-  },
-  methods: {
-    onSubmit () {
-      const { title, writer, content, files } = this
-      this.$emit('submit', { title, writer, content, files })
+    name: "NoticeRegisterForm",
+    data () {
+        return {
+            title: '제목을 입력하세요.',
+            Writer: 'WMC',
+            content: '내용을 입력하세요.',
+            files: [],
+        }
+    },
+    methods: {
+        onSubmit () {
+            const { title, writer, content, files } = this
+            this.$emit('submit', { title, writer, content, files })
+            
+        },
+        handleFileUpload(event) {
+          this.files = event.target.files;
+        },
     }
   }
-}
+
 </script>
 
 <style scoped>
--- src/views/csCenter/notice/NoticeRegisterPage.vue
+++ src/views/csCenter/notice/NoticeRegisterPage.vue
@@ -10,23 +10,23 @@
 import NoticeRegisterForm from '../../../components/csCenter/notice/NoticeRegisterForm.vue'
 import { mapActions } from 'vuex'
 
-const noticeModule = 'noticeModule'
+const noticeModule = "noticeModule";
 
 export default {
   components: { NoticeRegisterForm },
   name: 'NoticeListPage',
   methods: {
-    ...mapActions(noticeModule, [
-      'requestCreateNoticeToSpring'
-    ]),
-    async onSubmit (payload) {
-      await this.requestCreateNoticeToSpring(payload)
+    ...mapActions([`${noticeModule}/requestCreateNoticeToSpring`]),
+    async onSubmit(payload) {
+      await this[`${noticeModule}/requestCreateNoticeToSpring`](payload)
       await this.$router.push({
         name: 'NoticeListPage'
       })
     }
   }
-}
+};
+
+
 </script>
 
 <style></style>
