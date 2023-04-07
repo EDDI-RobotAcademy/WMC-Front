477a055dd89dabc724bb5c276615d134b4535b39
jonginhan <instantgogo@gmail.com>
2023-04-07 16:35:05 +0900

    fix: [한종인]상품 상세페이지에서 cart 추가시 localStorage에 productId 반영 안 되던 에러[WMC-UI 12]

--- src/components/order/CartForm.vue
+++ src/components/order/CartForm.vue
@@ -30,14 +30,20 @@
               ></v-checkbox>
             </td>
             <td>
-              <router-link :to="{ name: 'ProductDetailPage', params: { productId: item.product_id } }" tag="div">
-              <v-img
-                :src="item.image"
-                alt="Product image"
-                max-width="50"
-                max-height="50"
-              />
-            </router-link>
+              <router-link
+                :to="{
+                  name: 'ProductDetailPage',
+                  params: { productId: item.product_id },
+                }"
+                tag="div"
+              >
+                <v-img
+                  :src="item.image"
+                  alt="Product image"
+                  max-width="50"
+                  max-height="50"
+                />
+              </router-link>
             </td>
             <td>{{ item.name }}</td>
             <td>
@@ -66,58 +72,58 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 export default {
-  data () {
+  data() {
     return {
       cartItems: [],
       selectAll: false,
       shippingFee: 50,
-      sum: 0
-    }
+      sum: 0,
+    };
   },
-  created () {
-    const token = JSON.parse(localStorage.getItem('userInfo'))
+  created() {
+    const token = JSON.parse(localStorage.getItem('userInfo'));
     axios.post('http://localhost:7777/cart/validate', token).then((res) => {
       if (res.data) {
-        console.log('인증된 사용자 입니다.')
-        const [memberId, authorityName] = res.data.split(':')
-        console.log(memberId)
-        console.log(authorityName)
-        const cartKey = `cart_${memberId}`
+        console.log('인증된 사용자 입니다.');
+        const [memberId, authorityName] = res.data.split(':');
+        console.log(memberId);
+        console.log(authorityName);
+        const cartKey = `cart_${memberId}`;
         const cartItems = localStorage.getItem(cartKey)
           ? JSON.parse(localStorage.getItem(cartKey))
-          : []
-        this.cartItems = cartItems
+          : [];
+        this.cartItems = cartItems;
       }
-    })
+    });
   },
 
   watch: {
     cartItems: {
       deep: true,
-      handler () {
-        this.updateSum()
-      }
-    }
+      handler() {
+        this.updateSum();
+      },
+    },
   },
 
   methods: {
-    toggleAllItems () {
-      this.cartItems.forEach((item) => (item.selected = this.selectAll))
-      this.updateSum()
+    toggleAllItems() {
+      this.cartItems.forEach((item) => (item.selected = this.selectAll));
+      this.updateSum();
     },
-    removeSelectedItems () {
-      this.cartItems = this.cartItems.filter((item) => !item.selected)
+    removeSelectedItems() {
+      this.cartItems = this.cartItems.filter((item) => !item.selected);
     },
-    updateSum () {
+    updateSum() {
       this.sum = this.cartItems.reduce(
         (acc, item) => acc + (item.selected ? item.quantity * item.price : 0),
         0
-      )
-    }
-  }
-}
+      );
+    },
+  },
+};
 </script>
 
 <style scoped>
