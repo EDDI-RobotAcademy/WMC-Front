eb4f73f4ab0178fe988af45a6d8558d0312623de
Yoon Yongjin <abcd950217@gmail.com>
2023-04-10 16:49:33 +0900

    fix: [윤용진] 공지사항 등록 페이지 UI [WMC-UI 21]

---
+++ src/views/csCenter/notice/NoticeRegisterPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+      <h2>공지사항 작성</h2>
+      <notice-register-form @submit="onSubmit" />
+  </v-container>
+</template>
+
+<script>
+
+import NoticeRegisterForm from '../../../components/csCenter/notice/NoticeRegisterForm.vue'
+import { mapActions } from 'vuex'
+
+const noticeModule = "noticeModule";
+
+export default {
+components: { NoticeRegisterForm },
+name: 'NoticeListPage',
+methods: {
+  ...mapActions([`${noticeModule}/requestCreateNoticeToSpring`]),
+  async onSubmit(payload) {
+    await this[`${noticeModule}/requestCreateNoticeToSpring`](payload)
+    await this.$router.push({
+      name: 'NoticeListPage'
+    })
+  }
+}
+};
+
+
+</script>
+
+<style></style>
