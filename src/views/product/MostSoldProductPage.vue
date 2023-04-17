b16ecd52a3db86b09b2056ec7b168eb8ec0290df
jonginhan <instantgogo@gmail.com>
2023-04-17 17:24:22 +0900

    feat: [한종인] 메인화면에 상품 판매량 순으로 나열하는 인기상품 리스트 UI[WMC-UI 56]

---
+++ src/views/product/MostSoldProductPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="padding:24px;">
+    <br/>
+    <br/>
+    <h4 class="body-text">인기상품
+      <button>&nbsp;<&nbsp; </button>
+      <button> &nbsp;></button>
+    </h4>
+    <br/>
+      <div class="body-container">
+        <most-sold-product-list :products="mostSoldProducts"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import MostSoldProductList from '@/components/product/MostSoldProductList.vue';
+import { mapActions, mapState } from 'vuex'
+
+const productModule = 'productModule'
+
+export default {
+  name: 'MostSoldProductPage',
+  components: {
+    MostSoldProductList,
+  },
+  computed: {
+    ...mapState(productModule, ['mostSoldProducts']),
+    productItemList() {
+      return this.mostSoldProducts.map(product => ({
+        ...product,
+        imageDataList: product.imageDataList
+      }))
+    }
+  },
+  mounted () {
+    this.requestMostSoldProductListToSpring()
+  },
+  methods: {
+    ...mapActions(productModule, [
+      'requestMostSoldProductListToSpring'
+    ])
+  }
+};
+</script>
+
+<style></style>
