005ed2b369650bb0baede81e833919f843f8e2d8
gomgomehoya <gomgomehoya@gmail.com>
2023-04-25 21:15:34 +0900

    fix:[김호유] 에러수정
    에러수정

--- src/components/mypage/MyPageTab.vue
+++ src/components/mypage/MyPageTab.vue
@@ -1,7 +1,5 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="2">
         <v-tabs
           v-model="active_tab"
           background-color="white"
@@ -11,14 +9,11 @@
           <v-tab
             :key="i"
             v-for="(item, i) in navi"
-            v-on:click="currentTab = item.tab"
+            @click="$emit('tab-changed', item.tab)"
           >
             {{ item.menu }}
           </v-tab>
         </v-tabs>
-      </v-col>
-      
-    </v-row>
   </v-container>
 </template>
 
@@ -37,11 +32,11 @@ export default {
   },
   data() {
     return {
-      currentTab: "MyPageForm",
       active_tab: 0,
+      currentTab: "MyPageForm",
       navi: [
         { menu: "회원정보", tab: "MyPageForm" },
-        { menu: "찜한상품", tab: "CartForm" },
+      
       ],
     };
   },
@@ -49,13 +44,11 @@ export default {
     this.active_tab = this.index;
     console.log("navi : " + this.currentTab);
   },
-  computed: {
-    componentLoader() {
-      const tab = this.currentTab;
-      return () => import(`@/components/myPage/${tab}`);
+  methods: {
+    changeTab(tab) {
+      this.currentTab = tab;
     },
   },
-  methods: {},
 };
 </script>
 
--- src/views/myPage/MyPageView.vue
+++ src/views/myPage/MyPageView.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <div>
-      <my-page-tab/>
+  <div style="margin-top: 100px">
+    <div style="display: flex; padding: 30px">
+      <my-page-tab
+        style="flex-basis: 20%"
+        @tab-changed="currentTab = $event"
+      />
+      <div style="flex-basis: 80%">
+        <component :is="currentTab" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import MyPageTab from '../../components/myPage/MyPageTab.vue';
+import MyPageTab from '@/components/myPage/MyPageTab.vue';
+import MyPageForm from '@/components/myPage/MyPageForm.vue';
 import { mapActions } from 'vuex';
 
 const memberModule = 'memberModule';
@@ -16,6 +23,12 @@ export default {
   name: 'MyPageView',
   components: {
     MyPageTab,
+    MyPageForm,
+  },
+  data(){
+    return{
+      currentTab: 'MyPageForm'
+    }
   },
 
   mounted() {
