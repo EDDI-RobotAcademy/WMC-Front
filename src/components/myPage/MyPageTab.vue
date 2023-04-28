cb2317d1b118766b3b0ea62794870b93b259f2d2
gomgomehoya <gomgomehoya@gmail.com>
2023-04-28 17:31:35 +0900

    hotfix:[김호유] GIThub 내에서 생긴 경로 추가 생성 에러
    hotfix:[김호유] GIThub 내에서 생긴 경로 추가 생성 에러

---
+++ src/components/myPage/MyPageTab.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container>
+          <v-tabs
+            v-model="active_tab"
+            fixed-tabs
+            background-color="white"
+            color="#205C37"
+            vertical
+          >  
+            <v-tab
+              :key="i"
+              v-for="(item, i) in navi"
+              @click="$emit('tab-changed', item.tab)"
+            >
+              {{ item.menu }}
+            </v-tab>
+          </v-tabs>
+    </v-container>
+  </template>
+  
+  <script>
+  //경로가 왜이래
+  export default {
+    
+    name: 'MyPageTab',
+    props: {
+      name: {
+        type: String,
+        default:'',
+      },
+      index: {
+        type: Number,
+        default: 0,
+      },
+    },
+    data() {
+      return {
+        active_tab: 0,
+        currentTab: 'MyPageForm',
+        navi: [
+          { menu: '회원정보', tab: 'MyPageForm' },
+        
+        ],
+      };
+    },
+    mounted() {
+      this.active_tab = this.index;
+      console.log("navi : " + this.currentTab);
+    },
+    methods: {
+      changeTab(tab) {
+        this.currentTab = tab;
+      },
+    },
+  };
+  </script>
+  
+  
