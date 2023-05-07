ca0674dd75345aa8e6d42f150c68f555717dfe1b
jonginhan <instantgogo@gmail.com>
2023-05-07 22:47:38 +0900

    feat: [한종인] 사용자가 상품을 찜 (like)할 수 있는 기능 [WMC-UI 70]

--- src/views/product/ProductDetailPage.vue
+++ src/views/product/ProductDetailPage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <product-detail-form :product="product" />
+    <product-detail-form :product="product" :liked="liked" />
     <review-list :reviews="reviews"></review-list>
   </v-container>
 </template>
@@ -9,6 +9,7 @@
 import { mapActions, mapState } from 'vuex';
 import ProductDetailForm from '@/components/product/ProductDetailForm.vue';
 import ReviewList from '@/components/review/ReviewList.vue';
+import mainRequest from '@/api/mainRequest';
 
 const reviewModule = 'reviewModule';
 
@@ -20,11 +21,13 @@ export default {
   data() {
     return {
       product: null,
+      liked: false,
     };
   },
   mounted() {
     this.fetchProduct();
     this.fetchReviewList();
+    this.fetchLikedStatus();
   },
   methods: {
     ...mapActions('productModule', ['fetchProductById']),
@@ -37,11 +40,50 @@ export default {
       const productId = this.$route.params.productId;
       await this.requestReviewListToSpring(productId);
     },
+    async fetchLikedStatus() {
+      if (!this.isAuthenticated) {
+        return;
+      }
+
+      const memberId = localStorage.getItem('memberId');
+      const productId = this.$route.params.productId;
+
+      try {
+        const userInfo = localStorage.getItem('userInfo');
+        if (!userInfo) {
+          alert('사용자 정보를 찾을 수 없습니다');
+          return;
+        }
+
+        const token = JSON.parse(userInfo);
+        const response = await mainRequest.get(
+          `/member/liked/${memberId}/${productId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          this.liked = response.data;
+          console.log('liked:', this.liked);
+        } else {
+          alert('좋아요 상태를 가져오는데 실패했습니다. 다시 시도해주세요.');
+        }
+      } catch (error) {
+        console.error('Error while fetching liked status:', error);
+        alert('좋아요 상태를 가져오는데 실패했습니다. 다시 시도해주세요.');
+      }
+    },
   },
   computed: {
     ...mapState(reviewModule, ['reviews']),
     productId() {
       return this.$route.params.productId;
+    },
+    isAuthenticated() {
+      return !!localStorage.getItem('userInfo');
     },
   },
 };
